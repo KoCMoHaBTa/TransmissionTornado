056506b3bf2c68a59619e84b0d2b6a6615d09e47
Milen Halachev <milenski88@gmail.com>
2017-11-06 23:15:46 +0200

    - added plain macOS target - moved shared code and resources to separate folder

---
+++ macOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macOS
+//
+//  Created by Milen Halachev on 6.11.17.
+//  Copyright © 2017 KoCMoHaBTa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macOS
+//
+//  Created by Milen Halachev on 6.11.17.
+//  Copyright © 2017 KoCMoHaBTa. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
